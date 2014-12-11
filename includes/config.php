586206dc1c06606170f3628f16442376c2129108
Joseph Zhong <glub.bob@gmail.com>
2014-12-11 09:25:54 -0800

    Added Assets

---
+++ includes/config.php
@@ -0,0 +1,6 @@
+<?php
+	$GLOBALS["server"] = "mysql.lakeside-cs.org";
+	$GLOBALS["server_username"] = "lakesidecs"; // Mac users this should be root
+	$GLOBALS["password"] = "m760aCS4Fall14"; // Mac users this should be root
+	$GLOBALS["database"] = "fall14big";
+?>
---
+++ includes/localconfig.php
@@ -0,0 +1,6 @@
+<?php
+	$GLOBALS["server"] = "127.0.0.1";
+	$GLOBALS["server_username"] = "root"; 
+	$GLOBALS["password"] = ""; // Mac users this should be root
+	$GLOBALS["database"] = "fall14sensei";
+?>
