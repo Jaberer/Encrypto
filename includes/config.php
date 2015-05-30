e234f4a814651550718f933e3ac2d26a3777822b
Joseph Zhong <joseph.zhong@hotmail.com>
2015-05-30 16:43:32 -0700

    Delete config.php

--- includes/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-	$GLOBALS["server"] = "mysql.lakeside-cs.org";
-	$GLOBALS["server_username"] = "lakesidecs"; // Mac users this should be root
-	$GLOBALS["password"] = "m760aCS4Fall14"; // Mac users this should be root
-	$GLOBALS["database"] = "fall14big";
-?>
